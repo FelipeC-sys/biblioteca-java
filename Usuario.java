401283214db640c8912979440889b6a55254c6c7
Andrés F. Carvajal <andrezx00@gmail.com>
2026-03-08 23:30:59 -0500

    Implemente atributos a la clase

--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,16 @@
 public class Usuario {
-    
+
+    String documento;
+    String nombre;
+    String tipo;
+
+    public Usuario(String documento, String nombre, String tipo) {
+        this.documento = documento;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public void mostrar() {
+        System.out.println(documento + " - " + nombre + " - " + tipo);
+    }
 }
