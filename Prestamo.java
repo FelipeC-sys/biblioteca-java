05ed116c82ebfe6cb7d5801e30ecf4b3d0dbcb52
Andrés F. Carvajal <andrezx00@gmail.com>
2026-03-08 23:32:27 -0500

    Implmente atributos

---
+++ Prestamo.java
@@ -0,0 +1,10 @@
+public class Prestamo {
+
+    String isbnLibro;
+    String documentoUsuario;
+
+    public Prestamo(String isbnLibro, String documentoUsuario) {
+        this.isbnLibro = isbnLibro;
+        this.documentoUsuario = documentoUsuario;
+    }
+}
