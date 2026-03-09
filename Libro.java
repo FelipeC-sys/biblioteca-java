8dadae7443039902ca45824a2edac856c2e5712e
Andrés F. Carvajal <andrezx00@gmail.com>
2026-03-08 23:26:59 -0500

    Implemente atributos y metodo mostrar en la clase libro

--- Libro.java
+++ Libro.java
@@ -1,3 +1,20 @@
 public class Libro {
-    
+
+    String isbn;
+    String titulo;
+    String autor;
+    int anio;
+    boolean disponible;
+
+    public Libro(String isbn, String titulo, String autor, int anio) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.disponible = true;
+    }
+
+    public void mostrar() {
+        System.out.println(isbn + " - " + titulo + " - " + autor + " - " + anio + " - " + (disponible ? "Disponible" : "Prestado"));
+    }
 }
