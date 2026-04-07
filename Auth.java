e8a3b025c1f0355de29d410ffa14ca79448636c2
Andrés F. Carvajal <9felipegc@gmail.com>
2026-04-06 23:21:40 -0500

    Clase para el login

--- Auth.java
+++ Auth.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Auth {
-    
+
+    static String usuario = "admin";
+    static String password = "1234";
+
+    public static boolean login() {
+
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+
+        System.out.print("Usuario: ");
+        String u = sc.nextLine();
+
+        System.out.print("Password: ");
+        String p = sc.nextLine();
+
+        if (u.equals(usuario) && p.equals(password)) {
+            return true;
+        }
+
+        return false;
+    }
 }
